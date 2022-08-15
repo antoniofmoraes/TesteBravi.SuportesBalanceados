1b066163aefb3c04aea11bdd99ed7c3d91eb31f3
Antonio Felype Moraes <antonio.f.f.moraes@gmail.com>
2022-08-15 13:34:06 -0300

    Adicionar arquivos de projeto.

---
+++ TesteBravi.SuportesBalanceados/Program.cs
@@ -0,0 +1,39 @@
+﻿public class SuportesBalanceados
+{
+    static void Main(string[] args)
+    {
+        string entrada = "()";
+        Console.WriteLine(isValid(entrada));
+    }
+
+    static bool isValid(string str)
+    {
+        var stack = new Stack<char>();
+
+        stack.Push('z');
+
+        Dictionary<char, char> dict = new Dictionary<char, char>()
+        {
+            { '}', '{' },
+            { ']', '[' },
+            { ')', '(' },
+        };
+
+        foreach (var c in str)
+        {
+            if (dict.ContainsKey(c))
+            {
+                if (dict[c] != stack.Pop())
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                stack.Push(c);
+            }
+        }
+
+        return stack.Count() <= 1;
+    }
+}
